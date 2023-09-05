5d1f93e65a3f96a83ef6403844f09a86362d1371
SkyHigh Modifications <93752549+SkyHighModifications@users.noreply.github.com>
2023-09-05 14:46:31 +0100

    added fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "cerulean"
+game "gta5"
+version "1.0.0"
+description "FiveM Real Weather & Real Time"
+author "Jijamik"
+name "Smurfy @ SkyHigh Modifications"
+lua54 'yes'
+
+client_script "client.lua"
+
+server_script "server.lua"
+
+shared_script "config.lua"
+
