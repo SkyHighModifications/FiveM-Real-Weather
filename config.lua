c06b67be2a72651a8c8856ff6295a40c522049c8
SkyHigh Modifications <93752549+SkyHighModifications@users.noreply.github.com>
2023-09-05 15:45:52 +0100

    Added config.lua

---
+++ config.lua
@@ -0,0 +1,17 @@
+--Config.lua created by Smurfy @ SkyHigh Modifications 05/09/23
+Config = {
+
+    CityID = "2641689", -- https://bulk.openweathermap.org/sample/
+    ApiKey = "c67ad773870622c3699a639632c3351f", -- https://home.openweathermap.org/api_keys
+
+    DiscordWebHook = "",
+    AvatarUrl = "https://upload.wikimedia.org/wikipedia/en/thumb/f/f4/Met_Office.svg/1024px-Met_Office.svg.png",
+    BotUserName = "Met Office",
+    FooterImage = "https://upload.wikimedia.org/wikipedia/en/thumb/f/f4/Met_Office.svg/1024px-Met_Office.svg.png",
+    ThumbnailImage = "",
+
+    RealTime = true, -- Use IRL PC time in server
+    WeatherScripts = {
+        "vSync"
+    }, -- Add more? {"vSync, So on, So on"}
+}
