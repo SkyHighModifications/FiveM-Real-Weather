90a9b64db8da860efa8763dfe333afbc1ddddd4a
SkyHigh Modifications <93752549+SkyHighModifications@users.noreply.github.com>
2023-09-05 14:45:50 +0100

    Updated and recoded some of server.lua

--- server.lua
+++ server.lua
@@ -1,33 +1,62 @@
 --Made by Jijamik, feel free to modify
+--Modified by Smurfy @ SkyHigh Modifications 05/09/23
+local GetWeather = "http://api.openweathermap.org/data/2.5/weather?id="..Config.CityID.."&lang=fr&units=metric&APPID="..Config.ApiKey
 
+RegisterNetEvent("SHM:RealTime")
+AddEventHandler("SHM:RealTime", function()
+    if Config.RealTime == true then
+    time = GetTime()
+	TriggerClientEvent("SHM:RealTime", -1, time.d, time.h, time.m, time.s)
+    elseif Config.RealTime == false then
+        CancelEvent()
+    end
+end)
 
-local cityid = "5368361" -- Los Angeles
-local apikey = "Ici votre clé openweathermap"
-local GetWeather = "http://api.openweathermap.org/data/2.5/weather?id="..cityid.."&lang=fr&units=metric&APPID="..apikey
-
-function sendToDiscordMeteo (type, name,message,color)
-    local DiscordWebHook = "Webhook Discord"
-
-    local avatar = "Url de l'image"
-
-
-    local embeds = {
-        {
-
-            ["title"]=message,
-            ["type"]="rich",
-            ["color"] =color,
-            ["footer"]=  {
-                ["text"]= "-------------------------------------------------------------------------------------------------------------------",
-            },
-        }
-    }
-
-    if message == nil or message == '' then return FALSE end
-    PerformHttpRequest(DiscordWebHook, function(err, text, headers) end, 'POST', json.encode({ username = name,embeds = embeds,avatar_url = avatar}), { ['Content-Type'] = 'application/json' })
+function GetTime()
+        local timestamp = os.time()
+        local d = os.date("*t", timestamp).wday
+        local h = tonumber(os.date("%H", timestamp))
+        local m = tonumber(os.date("%M", timestamp))
+        local s = tonumber(os.date("%S", timestamp))
+	return {d = d, h = h, m = m, s = s}
 end
 
-function checkMeteo(err,response)
+Citizen.CreateThread(function()
+    for k, v in pairs(Config.WeatherScripts) do
+        local resource = v
+        local current = GetCurrentResourceName()
+        local author = "Jijamik"
+        local modified = "SkyHigh Modifications"
+        StopResource(current)
+        StopResource(resource)
+        print("^1Stopped this resource "..resource.. " or these resources "..resource..". ^3To prevent any conflict^7")
+        StartResource(current)
+        print("^2Restarted ^4"..current.. "^2 ENJOY^7")
+        print("^6Created by "..author.." and Modified by "..modified.."^7")
+   end
+end)
+
+
+function sendToDiscordForecast(type, color, name, message)
+      
+
+    local botreply = {
+          {
+              ["color"] = 008000,
+              ["title"] = "**".. GetCurrentResourceName() .."**",
+              ["type"] = type,
+              ["description"] = message,
+              ["footer"] = {
+                ["text"] = "Created By Jijamik & SkyHigh Modifications ",
+                ["icon_url"] = Config.FooterImage,
+            },
+          }
+      }
+      if message == nil or message == '' then return FALSE end
+    PerformHttpRequest(Config.DiscordWebHook, function(err, text, headers) end, 'POST', json.encode({username = Config.BotUserName, embeds = botreply, avatar_url = Config.AvatarUrl}), { ['Content-Type'] = 'application/json' })
+end
+
+function checkForecast(err,response)
     local data = json.decode(response)
     local type = data.weather[1].main
     local id = data.weather[1].id
@@ -41,58 +70,58 @@ function checkMeteo(err,response)
     local tempmaxi = math.floor(data.main.temp_max)
     local emoji = ":white_sun_small_cloud:"
     if type == "Thunderstorm" then
-        meteo = "THUNDER"
+        forecast = "THUNDER"
         emoji = ":cloud_lightning:"
     end
     if type == "Rain" then
-        meteo = "RAIN"
+        forecast = "RAIN"
         emoji = ":cloud_snow:"
     end
     if type == "Drizzle" then
-        meteo = "CLEARING"
+        forecast = "CLEARING"
         emoji = ":clouds:"
-        if id == 608  then
-            meteo = "OVERCAST"
+        if id == 608 then
+            forecast = "OVERCAST"
         end
     end
     if type == "Clear" then
-        meteo = "CLEAR"
+        forecast = "CLEAR"
         emoji = ":sun_with_face:"
     end
     if type == "Clouds" then
-        meteo = "CLOUDS"
+        forecast = "CLOUDS"
         emoji = ":clouds:"
-        if id == 804  then
+        if id == 804 then
             meteo = "OVERCAST"
         end
     end
     if type == "Snow" then
-        meteo = "SNOW"
+        forecast = "SNOW"
         emoji = ":cloud_snow:"
         if id == 600 or id == 602 or id == 620 or id == 621 or id == 622 then
-            meteo = "XMAS"
+            forecast = "XMAS"
         end
     end
 
     Data = {
-        ["Meteo"] = meteo,
+        ["forecast"] = forecast,
         ["VitesseVent"] = wind,
         ["DirVent"] = windrot
     }
-    TriggerClientEvent("meteo:actu", -1, Data)
-    sendToDiscordMeteo(1,('Météo'), emoji.." La météo à "..ville.." est "..description..". \n:thermometer: Il fait actuellement "..temp.."°C avec des minimales à "..tempmini.."°C et des maximales à "..tempmaxi.."°C. \n:wind_blowing_face: Des vents de "..wind.."m/s sont à prévoir.",16711680)
-    SetTimeout(60*60*1000, checkMeteoHTTPRequest)
+    TriggerClientEvent("forecast:actu", -1, Data)
+    sendToDiscordForecast(1,('Weather'), emoji.." The weather is "..ville.." est "..description..". \n:thermometer: Tempeture is currently "..temp.."°C minimum tempeture of "..tempmini.."°C maximum tempeture of "..tempmaxi.."°C. \n:wind_blowing_face: Winds of "..wind.."m/s are to be expected.",16711680)
+    SetTimeout(60*60*1000, checkForecastHTTPRequest)
 end
 
-function checkMeteoHTTPRequest()
-    PerformHttpRequest(GetWeather, checkMeteo, "GET")
+function checkForecastHTTPRequest()
+    PerformHttpRequest(GetWeather, checkForecast, "GET")
 end
 
-checkMeteoHTTPRequest()
+checkForecastHTTPRequest()
 
-RegisterServerEvent("meteo:sync")
-AddEventHandler("meteo:sync",function()
-    TriggerClientEvent("meteo:actu", source, Data)
+RegisterServerEvent("forecast:sync")
+AddEventHandler("forecast:sync",function()
+    TriggerClientEvent("forecast:actu", source, Data)
 end)
 
 --[[
