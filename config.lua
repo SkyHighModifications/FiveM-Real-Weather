14351bc2dfa4e4df30c08a27f406229a6c600854
SkyHigh Modifications <93752549+SkyHighModifications@users.noreply.github.com>
2023-09-08 03:35:26 +0100

    Updated config.lua
    Added code and removed a few

--- config.lua
+++ config.lua
@@ -1,13 +1,16 @@
 --Config.lua created by Smurfy @ SkyHigh Modifications 05/09/23
 Config = {
 
-    CityID = "2641689", -- https://bulk.openweathermap.org/sample/
-    ApiKey = "c67ad773870622c3699a639632c3351f", -- https://home.openweathermap.org/api_keys
+    CityID = "", -- https://bulk.openweathermap.org/sample/
+    ApiKey = "", -- https://home.openweathermap.org/api_keys
+    Country = "GB", -- Great Britian
 
     DiscordWebHook = "",
     AvatarUrl = "https://upload.wikimedia.org/wikipedia/en/thumb/f/f4/Met_Office.svg/1024px-Met_Office.svg.png",
     BotUserName = "Met Office",
     FooterImage = "https://upload.wikimedia.org/wikipedia/en/thumb/f/f4/Met_Office.svg/1024px-Met_Office.svg.png",
+    UpdateTimeLog = 900000, -- 15Mins
+    DiscordEmbedColor = 52548, -- Decimal Only
 
     RealTime = true, -- Use IRL PC time in server
     WeatherScripts = {
