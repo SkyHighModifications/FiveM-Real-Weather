8d94af04250d49a3e66ec6fff4d11f6afa59d905
SkyHigh Modifications <93752549+SkyHighModifications@users.noreply.github.com>
2023-09-19 22:24:56 +0100

    Update server.lua
    version added back

--- server.lua
+++ server.lua
@@ -331,3 +331,30 @@ function InfoHandle(msg, col)
     end
     print(("[^9INFO^7] ^3" .. GetCurrentResourceName() .. "^7: ^" .. col .. "%s^7"):format(msg))
 end
+
+if Config.CheckForUpdates then
+    local function VersionLog(_type, log)
+        local color = _type == 'success' and '^2' or '^1'
+        print(('^8['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
+    end
+
+    local function CheckMenuVersion()
+        PerformHttpRequest('https://raw.githubusercontent.com/SkyHighModifications/FiveM-WeatherSync-Pro/master/version.txt', function(err, text, headers)
+            local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+            if not text then
+                VersionLog('error', 'Currently unable to run a version check.')
+                return
+            end
+            if text:gsub("%s+", "") == currentVersion:gsub("%s+", "") then
+                VersionLog('success', 'You are running the latest version.')
+                VersionLog('success', ('Current Version: %s'):format(currentVersion))
+            else
+                VersionLog('error', ('You are currently running an outdated version, please update to version %s'):format(text))
+                VersionLog('success', ('Latest Version: %s'):format(text))
+            end
+        end)
+    end
+
+    CheckMenuVersion()
+end
+
